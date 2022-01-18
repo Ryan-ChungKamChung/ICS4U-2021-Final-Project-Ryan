1d11f4181f83307f582a0398fd56004d546d9d4c
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2022-01-18 12:31:01 -0500

    Changed API level, made the app full-screen and improved layouts

--- app/src/main/java/com/ryan/trivia_app/GameActivity.kt
+++ app/src/main/java/com/ryan/trivia_app/GameActivity.kt
@@ -7,6 +7,7 @@
 package com.ryan.trivia_app
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.ryan.trivia_app.databinding.ActivityGameBinding
 
@@ -26,5 +27,21 @@ class GameActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentPlaceholder, CategoriesFragment())
             .commit()
+    }
+
+    // Will fix later if needed, deprecation here isn't much of a concern
+    @Suppress("DEPRECATION")
+    override fun onResume() {
+        super.onResume()
+
+        // Removes top and bottom system bars
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+        /* Makes it so user clicks don't bring back the bars.
+           Minimum is API 19 so checking isn't needed */
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
     }
 }
