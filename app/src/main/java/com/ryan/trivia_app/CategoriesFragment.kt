2f1d757b00556aabb7ceba3f69a0433aaf733eca
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2022-01-20 21:02:40 -0500

    Fixed import

--- app/src/main/java/com/ryan/trivia_app/CategoriesFragment.kt
+++ app/src/main/java/com/ryan/trivia_app/CategoriesFragment.kt
@@ -1,7 +1,7 @@
 package com.ryan.trivia_app
 
-import android.graphics.Color
 import android.os.Bundle
+import android.graphics.Color
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
